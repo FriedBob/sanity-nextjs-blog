ba2b42d65335ac13f8cc618c7d51e5e2f67686ba
FriedBob <isshin95@gmail.com>
2022-08-05 18:43:26 +0900

    slug 추출

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,10 +1,14 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import sanityClient from "@sanity/client";
+import { sanityQuery } from "../../queries";
 
-const PostAll = () => {
-  const router = useRouter();
+interface Props {
+  slug: string;
+}
 
-  const { slug } = router.query;
-
+const PostAll = ({ slug }: Props) => {
   return (
     <div>
       <h1>PostAll</h1>
@@ -14,3 +18,35 @@ const PostAll = () => {
 };
 
 export default PostAll;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Sanity 로부터 데이터를 가져옴
+  const client = sanityClient({
+    dataset: "production",
+    projectId: "fty5c0wl",
+    useCdn: process.env.NODE_ENV === "production",
+  });
+
+  const homeQuery = sanityQuery.homeQuery;
+  const posts = await client.fetch(homeQuery);
+
+  const paths = posts.map((post: any) => ({
+    params: {
+      slug: post.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug }: any = params;
+  return {
+    props: {
+      slug,
+    },
+  };
+};
