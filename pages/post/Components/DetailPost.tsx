0893b406a33af6b38362fe2b3de25c5a10c3225b
FriedBob <isshin95@gmail.com>
2022-08-09 14:52:07 +0900

    sanity-block-content-to-react typescript 호환파일

--- pages/post/Components/DetailPost.tsx
+++ pages/post/Components/DetailPost.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { ReactPropTypes } from "react";
+import SanityBlockContent from "@sanity/block-content-to-react";
+
+interface Content {
+  blocks: Array<ContentProps>;
+}
+interface ContentProps {
+  _key: string;
+  _type: string;
+  children: Array<any>;
+  style: string;
+  markDefs: Array<any>;
+}
 
 /**
  * Post의 Content 내용물
  *
  */
-const DetailPost = () => {
+const DetailPost = ({ blocks }: Content) => {
   return <></>;
 };
 
