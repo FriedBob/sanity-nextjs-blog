7b8e0687e80f93fd2f10781c00a8d2d19b3c26e1
FriedBob <isshin95@gmail.com>
2022-08-10 17:37:43 +0900

    atom 생성

---
+++ pages/states/dataStore.ts
@@ -0,0 +1,25 @@
+// ------------------- useRecoilState 를 사용하는 전역 data값 저장소 ----------------------
+
+import { atom } from "recoil";
+
+interface PostProps {
+  author: { image: string; name: string; role: string }; // 글 작성자
+  createdAt: string; // 글 작성시기
+  slug: string; // 포스트 고유값 (pathname)
+  subtitle: string; // 글 부제
+  thumbnail: { alt: string; imageUrl: string };
+  title: string;
+  tag: TagProps; // 게시물 태그
+}
+
+interface TagProps {
+  title: string;
+  slug: string;
+}
+
+// Post들을 담고있는 배열 state
+export const postsState = atom<Array<PostProps>>({
+  key: "posts",
+
+  default: [],
+});
