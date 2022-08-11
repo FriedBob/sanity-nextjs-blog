75354504b93fc3c81d5ee2de0dda782ca702a7d8
FriedBob <isshin95@gmail.com>
2022-08-11 17:04:42 +0900

    "deploy to gh-pages"

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://FriedBob.github.io/sanity-nextjs-blog"
+      : "",
   reactStrictMode: false,
   swcMinify: true,
   env: {
